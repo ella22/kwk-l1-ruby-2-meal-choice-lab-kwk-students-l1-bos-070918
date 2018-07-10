06cb8388c2468a9a9d3af7dd9463933d4b280225
Ella Sklar <ella.soccer22@gmail.com>
2018-07-10 17:38:42 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,22 +1,22 @@
 
-def snacks(food="Cheetos")
+def snacks(food1="Cheetos")
   "Any time, is the right time for #{food}!"
 end
 puts snacks
 
 
-def breakfast(food="Frosted Flakes")
+def breakfast(food2="Frosted Flakes")
   "Morning is the best time for Frosted Flakes!"
 end
 puts breakfast
 
-def lunch(food="Grilled Chesse")
+def lunch(food3="Grilled Chesse")
   "Afternoon is the best time for Grilled Chesse!"
 end
 puts lunch
 
 
-def dinner(food="salmon")
+def dinner(food4="salmon")
   "Dinner is the best time for Salmon!"
 end 
   puts dinner
