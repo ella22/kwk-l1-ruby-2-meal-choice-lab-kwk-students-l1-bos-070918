e2a586b1ca52f5a951f3ec4d1d9e15a7fc65f905
Ella Sklar <ella.soccer22@gmail.com>
2018-07-10 17:42:54 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -10,7 +10,7 @@ def breakfast(food_2="frosted flakes")
 end
  puts breakfast
 
-def lunch(food_3="grilled Chesse")
+def lunch(food_3="grilled chesse")
   "Afternoon is the best time for #{food_3}!"
 end
 puts lunch
